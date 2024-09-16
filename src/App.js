826b53b82283d998cb31ec4aace42895983ae8a1
Romit Soni <romitsoni2003@gmail.com>
2024-09-16 18:05:47 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import EditorPage from './pages/EditorPage';
+import { Toaster } from 'react-hot-toast';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Toaster position='top-center'/>
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/editor/:roomId" element={<EditorPage />} />
+      </Routes>
+    </>
   );
 }
 
